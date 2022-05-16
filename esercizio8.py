c5d1389fd3d4ce79fad738d93cf92123e72f62cd
MattiaMarchese <105633037+MattiaMarchese@users.noreply.github.com>
2022-05-16 12:06:25 +0200

    Add files via upload

---
+++ esercizio8.py
@@ -0,0 +1,7 @@
+nome=input("inserire un nome:")
+print("nome: "+nome)
+print("contacaratteri:")
+num=len(nome)
+#conversione intero a stringa
+num=str(num)
+print("il tuo nome ha: "+num+" caratteri")
